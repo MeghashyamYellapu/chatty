348b3fa2828666aa0a5efbee4c8992cc6ddc1325
MeghaShyamy <shyamyellapu1@gmail.com>
2025-07-23 20:16:52 +0530

    changed controllers

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -16,7 +16,7 @@ export const getUsersForSidebar = async (req, res) => {
   }
 };
 
-export const getMessages = async (req, res) => {
+export const getmessages = async (req, res) => {
   try {
     const { id: userToChatId } = req.params;
     const myId = req.user._id;
