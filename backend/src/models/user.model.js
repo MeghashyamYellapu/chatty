fa9c99c78209c6eccec3b728d5b57c42feef453c
MeghaShyamy <shyamyellapu1@gmail.com>
2025-07-23 21:41:50 +0530

    fixed

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,30 +1,29 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    fullname:{
-        type:String,
-        required:true,
+    fullName: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:6,
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
     },
-    profilepic:{
-        type:String,
-        default:"",
-
+    profilePic: {
+      type: String,
+      default: "",
     },
-},
-{timestamps:true}
-)
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("user",userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,12 @@
-import express from "express"
-import { protectRoute } from "../middleware/auth.middleware.js"
-import { getmessages, getusersforsidebar, sendmessage } from "../controllers/message.controller.js"
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/users",protectRoute,getusersforsidebar)
-router.get("/:id",protectRoute,getmessages)
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
-router.post("/send/:id",protectRoute,sendmessage)
+router.post("/send/:id", protectRoute, sendMessage);
 
-export default router
+export default router;
